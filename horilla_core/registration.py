12e421144721bb33e9e53a08d988c9fb07c3682d
Horilla <support@horilla.com>
2026-01-30 17:12:14 +0530

    [UPDT] CORE: Updated registration for use auth model instead directly use HorillaUser

--- horilla_core/registration.py
+++ horilla_core/registration.py
@@ -2,14 +2,16 @@
 Feature registration for Horilla Core app.
 """
 
+from horilla.auth.models import User
 from horilla.registry.feature import register_models_for_feature
+from horilla_core.models import Company, Department, Role
 
 register_models_for_feature(
     models=[
-        ("horilla_core", "Company"),
-        ("horilla_core", "Department"),
-        ("horilla_core", "Role"),
-        ("horilla_core", "HorillaUser"),
+        Company,
+        Department,
+        Role,
+        User,
     ],
     all=True,
     exclude=["dashboard_component", "report_choices"],
