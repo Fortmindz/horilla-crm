412f35fb65bb63237bd023ba118048024618bf49
Horilla <support@horilla.com>
2026-04-10 17:48:18 +0530

    [UPDT] SETTINGS: Switch Channels backend from InMemoryChannelLayer to RedisChannelLayer

--- horilla/settings/base.py
+++ horilla/settings/base.py
@@ -180,7 +180,7 @@ ASGI_APPLICATION = "horilla.asgi.application"
 # -----------------------------------------------------------------------------
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
         # "CONFIG": {
         #     "hosts": [("127.0.0.1", 6379)],  # Redis server
         # },
